f776431781d0b3ed0a32821726a1cfb6ca5c9ea3
ivanov.igor <ivanov.igor.it@gmail.com>
2019-10-10 17:07:19 +0300

    video

--- vk1/view/video/Video_ViewController.swift
+++ vk1/view/video/Video_ViewController.swift
@@ -2,12 +2,13 @@ import UIKit
 
 class Video_ViewController: UIViewController {
 
+    @IBOutlet weak var bkgView: MyView_GradiendBackground!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let recognizer = UIPanGestureRecognizer(target: self,
                                                              action: #selector(handleGesture(_:)))
-        self.view.addGestureRecognizer(recognizer)
+        self.bkgView.addGestureRecognizer(recognizer)
     }
     
     @objc func handleGesture(_ recognizer: UIScreenEdgePanGestureRecognizer) {
@@ -17,9 +18,10 @@ class Video_ViewController: UIViewController {
                
            case .changed:
                let translation = recognizer.translation(in: recognizer.view)
-               let relativeTranslation = translation.x / (recognizer.view?.bounds.width ?? 1)
+               let relativeTranslation = translation.y / (recognizer.view?.bounds.height ?? 1)
                let progress = max(0, min(1, relativeTranslation))
-               if progress > 0.33 {
+               if progress > 0.2 {
+                //navigationController?.popViewController(animated: true)
                    dismiss(animated: true, completion: nil)
                }
            default: return
