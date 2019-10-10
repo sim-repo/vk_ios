49b1d63f1605e151796660fd6bdabf7935072b36
ivanov.igor <ivanov.igor.it@gmail.com>
2019-10-10 15:36:21 +0300

    transition1

--- vk1/view/group/my/MyGroupDetail_ViewController.swift
+++ vk1/view/group/my/MyGroupDetail_ViewController.swift
@@ -9,32 +9,34 @@ class MyGroupDetail_ViewController: UIViewController {
     @IBOutlet weak var image_orig_top_con: NSLayoutConstraint!
     @IBOutlet weak var image_orig_lead_con: NSLayoutConstraint!
     @IBOutlet weak var image_orig_trail_con: NSLayoutConstraint!
-    
-    @IBOutlet weak var textView_orig_trail_con: NSLayoutConstraint!
-    @IBOutlet weak var textView_orig_top_con: NSLayoutConstraint!
-    @IBOutlet weak var textView_orig_lead_con: NSLayoutConstraint!
-    @IBOutlet weak var textView_orig_height_con: NSLayoutConstraint!
+    @IBOutlet weak var image_orig_aspect: NSLayoutConstraint!
     
     
+   // @IBOutlet weak var textView_orig_trail_con: NSLayoutConstraint!
+   // @IBOutlet weak var textView_orig_top_con: NSLayoutConstraint!
+  //  @IBOutlet weak var textView_orig_lead_con: NSLayoutConstraint!
+  //  @IBOutlet weak var textView_orig_height_con: NSLayoutConstraint!
     
-    @IBOutlet weak var label_orig_bottom_con: NSLayoutConstraint!
-    @IBOutlet weak var label_orig_lead_con: NSLayoutConstraint!
-    @IBOutlet weak var label_orig_trail_con: NSLayoutConstraint!
+    
+  //  @IBOutlet weak var label_orig_height_con: NSLayoutConstraint!
+   // @IBOutlet weak var label_orig_bottom_con: NSLayoutConstraint!//
+   // @IBOutlet weak var label_orig_lead_con: NSLayoutConstraint!
+   // @IBOutlet weak var label_orig_trail_con: NSLayoutConstraint!
     
     @IBOutlet weak var image_mini_width1_con: NSLayoutConstraint!
     @IBOutlet weak var image_mini_top_con: NSLayoutConstraint!
     @IBOutlet weak var image_mini_bottom_con: NSLayoutConstraint!
     @IBOutlet weak var image_mini_lead_con: NSLayoutConstraint!
  
+
+   // @IBOutlet weak var label_mini_centerY_con: NSLayoutConstraint!
+   // @IBOutlet weak var label_mini_lead_con: NSLayoutConstraint!
+   // @IBOutlet weak var label_mini_trail_con: NSLayoutConstraint!
+   // @IBOutlet weak var textView_mini_trail_con: NSLayoutConstraint!
+   // @IBOutlet weak var textView_mini_lead_con: NSLayoutConstraint!
+   // @IBOutlet weak var textView_mini_height_con: NSLayoutConstraint!
+   // @IBOutlet weak var textView_mini_bottom_con: NSLayoutConstraint!
     
-    @IBOutlet weak var label_mini_aspect_con: NSLayoutConstraint!
-    @IBOutlet weak var label_mini_lead_con: NSLayoutConstraint!
-    @IBOutlet weak var label_mini_trail_con: NSLayoutConstraint!
-    @IBOutlet weak var label_mini_top_con: NSLayoutConstraint!
-    @IBOutlet weak var textView_mini_trail_con: NSLayoutConstraint!
-    @IBOutlet weak var textView_mini_top_con: NSLayoutConstraint!
-    @IBOutlet weak var textView_mini_lead_con: NSLayoutConstraint!
-    @IBOutlet weak var textView_mini_height_con: NSLayoutConstraint!
     @IBOutlet weak var primaryView: MyView_Primary!
     @IBOutlet weak var bkgView: MyView_GradiendBackground!
     @IBOutlet var mainView: MyView_SecondaryDark!
@@ -53,31 +55,32 @@ class MyGroupDetail_ViewController: UIViewController {
         super.viewWillDisappear(animated)
         let navigationCtl = navigationController as! ZoomNavigatorController
         navigationCtl.popAnimator.presenting = false
+        nameLabel.removeFromSuperview()
+        descTextView.removeFromSuperview()
         
         UIView.animate(withDuration: 2) {
+            self.image_orig_aspect.isActive = false
             self.image_orig_top_con.isActive = false
             self.image_orig_lead_con.isActive = false
             self.image_orig_trail_con.isActive = false
-            self.textView_orig_trail_con.isActive = false
-            self.textView_orig_top_con.isActive = false
-            self.textView_orig_lead_con.isActive = false
-           // self.textView_orig_height_con.constant = 30
-            self.label_orig_bottom_con.isActive = false
-            self.label_orig_lead_con.isActive = false
-            self.label_orig_trail_con.isActive = false
+           // self.textView_orig_trail_con.isActive = false
+           // self.textView_orig_top_con.isActive = false
+          //  self.textView_orig_lead_con.isActive = false
+           
+           // self.label_orig_bottom_con.isActive = false
+         //   self.label_orig_lead_con.isActive = false//
+          //  self.label_orig_trail_con.isActive = false
 
             self.image_mini_top_con.isActive = true
             self.image_mini_bottom_con.isActive = true
             self.image_mini_lead_con.isActive = true
             self.image_mini_width1_con.isActive = true
 
-
-            self.label_mini_aspect_con.isActive = true
-            self.label_mini_lead_con.isActive = true
-            self.label_mini_top_con.isActive = true
-            self.textView_mini_top_con.isActive = true
-            self.textView_mini_lead_con.isActive = true
-            self.textView_mini_height_con.isActive = true
+          //  self.label_mini_centerY_con.isActive = true
+          //  self.label_mini_lead_con.isActive = true
+         //   self.textView_mini_lead_con.isActive = true
+         //   self.textView_mini_height_con.isActive = true
+          //  self.textView_mini_bottom_con.isActive = true
             self.primaryView.alpha = 1.0
             self.bkgView.alpha = 0
             self.view.layoutIfNeeded()
